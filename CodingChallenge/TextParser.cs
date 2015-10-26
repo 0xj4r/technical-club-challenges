146bfa056c15f3a2faac45514e3b91ce61c898b9
Josh Ransom <josh.bh.ransom@gmail.com>
2015-10-26 16:24:36 -0700

    Added text parsing functionality

---
+++ CodingChallenge/TextParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions; 
+
+namespace CodingChallenge
+{
+    public class TextParser
+    {
+        public TextParser()
+        {
+
+        }
+
+        public string Parse(string tempString)
+        {
+            var localString = tempString;
+            return localString; 
+        }
+
+        public List<string> ParseString(string tempString)
+        {
+            var cleanString = Regex.Replace(tempString, "[^0-9a-zA-Z']+", " ");
+            return cleanString.Split().Distinct().ToList(); 
+        }
+
+        public List<string> ParseSortedList(string listString)
+        {
+            List<string> parsedList = ParseString(listString);
+            parsedList.Sort();
+            return parsedList; 
+        }
+
+      
+    }
+}
---
+++ CodingChallengeTest/TextParserTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge;
+using System.Collections.Generic; 
+namespace CodingChallengeTest
+{
+    [TestClass]
+    public class TextParserTests
+    {
+        [TestMethod]
+        public void TestReadString()
+        {
+            var runner = new TextParser(); 
+            var tempString = "this"; 
+            var parsedString = runner.Parse(tempString); 
+            Assert.AreEqual(tempString, parsedString);
+        }
+
+        [TestMethod]
+        public void ParseList()
+        {
+            var runner = new TextParser();
+            var listString = "first second third"; 
+            var parsedList = runner.ParseString(listString); 
+            Assert.AreEqual(parsedList.Count, 3);
+            Assert.AreEqual(parsedList[1], "second");     
+        }
+
+        [TestMethod]
+        public void ParseWithDelimeters()
+        {
+            var runner = new TextParser();
+            var listString = @"first, second, / \ + = . , & third";
+            var parsedList = runner.ParseString(listString);
+            Assert.AreEqual(3, parsedList.Count);
+            Assert.AreEqual(parsedList[1], "second");
+        }
+        
+        [TestMethod]
+        public void ParseStringsWithApostraphe()
+        {
+            var runner = new TextParser();
+            var listString = "we're coding";
+            var parsedList = runner.ParseString(listString);
+            Assert.AreEqual(2, parsedList.Count);
+            Assert.AreEqual(parsedList[0], "we're");
+        }
+
+        [TestMethod]
+        public void ParseSortedList()
+        {
+            var runner = new TextParser();
+            var listString = "we're coding";
+            var parsedList = runner.ParseSortedList(listString);
+            Assert.AreEqual(2, parsedList.Count);
+            Assert.AreEqual(parsedList[1], "we're");
+        }
+
+        [TestMethod]
+        public void ParseUniqueList()
+        {
+            var runner = new TextParser();
+            var listString = "we're coding coding coding we're";
+            var parsedList = runner.ParseSortedList(listString);
+            Assert.AreEqual(2, parsedList.Count);
+            Assert.AreEqual(parsedList[1], "we're");
+        }
+
+    }
+}
