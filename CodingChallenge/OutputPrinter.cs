3d100890be6b281e39c62f8c7d19cc893d4ed85b
Josh Ransom <josh.bh.ransom@gmail.com>
2015-10-26 16:26:58 -0700

    OutputPrinter Class: Adds functionality for printing parsed list out to files

---
+++ CodingChallenge/OutputPrinter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace CodingChallenge
+{
+    public class OutputPrinter
+    {
+        public OutputPrinter()
+        {
+
+        }
+
+        public void printOutputList(List<string> inputFileList, string inputFileId)
+        {
+            var inputId = Int32.Parse(inputFileId.Substring(6));
+            var outputWriter = new StreamWriter(@"C:\temp\output_" + inputId + ".txt");
+            foreach (string s in inputFileList)
+            {
+                outputWriter.WriteLine(s);
+            }
+
+            outputWriter.Close(); 
+
+        }
+    }
+}
---
+++ CodingChallengeTest/OutputPrinterTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using CodingChallenge; 
+
+
+namespace CodingChallengeTest
+{
+    /// <summary>
+    /// Summary description for OutputPrinterTests
+    /// </summary>
+    [TestClass]
+    public class OutputPrinterTests
+    {
+        public OutputPrinterTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void WriteOutputFile()
+        {
+            var inputFileId = "input_1"; 
+            var printer = new OutputPrinter();
+            var inputFileList = new List<string> { "test", "input", "text" }; 
+            printer.printOutputList(inputFileList, inputFileId);
+            var reader = new StreamReader(@"C:\temp\output_1.txt");
+            var outputText = reader.ReadToEnd();
+            reader.Close();
+            Assert.AreEqual("test\r\ninput\r\ntext\r\n", outputText);
+            File.Delete(@"C:\temp\output_1.txt"); 
+        }
+    }
+}
